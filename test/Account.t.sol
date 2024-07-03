0ce237314d7687c0b4e79fb8d8fdaba54afa3abe
RonexLemon <ronexondimu@gmail.com>
2024-07-03 10:12:59 +0300

    user details

--- test/Account.t.sol
+++ test/Account.t.sol
@@ -10,10 +10,15 @@ contract BankAccountTest is Test{
     event  Withdraw (address _withdrawer,uint256 _amount,uint256 _time);
     event  Send(address _from, address _to, uint256 _amount, uint256 _time);
     event  CloseAccount(address _owner,uint256 _balance,uint256 _time);
+
     BankAccount public bankaccount;
+    address public Alice = address(1);
+    address public Bob = address(2);
+    
 
     function setUp()public{
         bankaccount = new BankAccount();
+        
     }
 
     function test_CreateAccount()public{
@@ -22,5 +27,28 @@ contract BankAccountTest is Test{
         emit CreateAccount(address(this), 0.2 ether, currentTime);
         bankaccount.createAccount{value:0.2 ether}();
     }
+
+    function test_CheckUserDetails()public{
+        uint256 currentTime = block.timestamp;
+
+             // Create account first
+           
+            
+        bankaccount.createAccount{value: 0.2 ether}();
+
+        // Get user details
+       (address owner, uint256 balance,uint256 createdTime ,bool isopen) = bankaccount.account(address(this));
+
+        // Check balance
+        assertEq(owner, address(this));
+         assertEq(balance, 0.2 ether);
+         assertEq(createdTime,currentTime);
+         assertEq(isopen,true);
+        
+    }
+
+    
+
+
 }
 
